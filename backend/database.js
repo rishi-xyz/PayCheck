4b760f8b604702a370083315ca1b4ab7329a7c68
rishi-xyz <itsranahrishikesh@gmail.com>
2024-08-25 12:01:16 +0530

    initial commit

---
+++ backend/database.js
@@ -0,0 +1,57 @@
+/**
+ * steps:
+ * 1.import mongoose
+ * 2.connect to mongodb
+ * 3.defineschema
+ * 4.create model with that schema
+ * 5.export
+ */
+const mongoose = require("mongoose");
+
+//Connect to mongodb
+mongoose.connect("mongodb+srv://itsranahrishikesh:BvFaj7rhKgifcr8v@cluster0.reeymeq.mongodb.net/paycheck");
+
+//Create a Schema for Users
+//Simple solution
+// const UserSchema = new mongoose.Schema({
+//     username:String,
+//     password:String,
+//     firstname:String,
+//     lastname:String
+// });
+//More Elegant solution
+const UserSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+        trim:true, //remove white spaces from both end of string
+        minlength:3,
+        maxlength:30
+    },
+    password:{
+        type:String,
+        required:true,
+        minlength:6
+    },
+    firstname:{
+        type:String,
+        required:true,
+        trim:true,
+        maxlength:50
+    },
+    lastname:{
+        type:String,
+        required:true,
+        trim:true,
+        maxlength:50
+    }
+});
+
+//Create a model from the Schema
+const User = mongoose.model('User',UserSchema);
+
+//Export user
+module.exports = {
+    User
+};
---
+++ backend/routes/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+//Single routing
+const router = express.Router();
+
+// Multiple routing
+// const router1 = express.Router();
+// const router2 = express.Router();
+// const router3 = express.Router();
+
+module.exports = router;
