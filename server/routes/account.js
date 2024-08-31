0645aff855d6b68a4e5278265de203ff37947f99
rishi-xyz <itsranahrishikesh@gmail.com>
2024-08-31 12:43:00 +0530

    modified

--- server/routes/account.js
+++ server/routes/account.js
@@ -22,7 +22,7 @@ AccountRouter.get("/balance",authMiddleware,async (req,res)=>{
 });
 
 const TransferSchema = z.object({
-    to:z.string().email(),
+    to:z.string(),
     amount:z.number().positive()
 }); 
 
@@ -30,14 +30,17 @@ AccountRouter.post("/transfer",authMiddleware,async(req,res)=>{
     const ParsedBody = TransferSchema.safeParse(req.body);
     if(!ParsedBody.success){
         return res.status(400).json({
-            message:"Incorrect Inputs"
+            message:"Incorrect Inputs",
+            error:ParsedBody.error
         });
     }
     const { to: To, amount: Amount } = ParsedBody.data;
     const TransactionSession = await mongoose.startSession();
     TransactionSession.startTransaction();
+    console.log("checkpoint 1");
     try{
         const UserAccount = await FetchAccountById(req.authenticateduserId);
+        console.log("checkpoint 2");
         if(!UserAccount){
             throw new Error("Account not Found");
         }
@@ -46,18 +49,22 @@ AccountRouter.post("/transfer",authMiddleware,async(req,res)=>{
                 message:"Insufficient balance"
             });
         }
+        console.log("checkpoint 3");
         const ToAccount = await FetchAccountById(To);
+        console.log("checkpoint 4");
         if(!ToAccount){
             throw new Error("Invalid Account");
         }
-        await Account.updateOne({UserId:req.authenticateduserId},{$inc:{balance:-Amount}}).session(TransactionSession);
-        await Account.updateOne({UserId:To},{$inc:{balance:Amount}}).session(TransactionSession);
+        console.log("checkpoint 5");
+        await Account.updateOne({UserId:req.authenticateduserId},{$inc:{balance:(balance-Amount)}}).session(TransactionSession);
+        console.log("checkpoint 6");
+        await Account.updateOne({UserId:To},{$inc:{balance:balance+Amount}}).session(TransactionSession);
+        console.log("checkpoint 7");
         await TransactionSession.commitTransaction();
-
+        console.log("checkpoint 8");
         return res.status(200).json({
             message:"Transfer Successful"
         });
-        
     }catch(error){
         await TransactionSession.abortTransaction();
         return res.status(400).json({message:error.message || "Transfer failed"});
@@ -67,8 +74,11 @@ AccountRouter.post("/transfer",authMiddleware,async(req,res)=>{
 });
 
 async function FetchAccountById(UserId){
+    console.log("userid input is =",UserId)
     const ObjId = new mongoose.Types.ObjectId(UserId);
+    console.log("objid =",ObjId)
     const Accountfetched = await Account.findOne({ userId: ObjId });
+    console.log("Account fetched is = ",Accountfetched)
     return Accountfetched;
 }
 
